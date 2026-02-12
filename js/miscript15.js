ed5946ad2c4e38bad5085a52a835f7073c5b1ff7
tanianap <qyj26808@educastur.es>
2026-02-12 10:33:55 +0100

    Ejercicio 15 JS

---
+++ js/miscript15.js
@@ -0,0 +1,7 @@
+var mes=prompt("Dime un mes","");
+
+if(mes.toLowerCase()=="agosto"){
+    document.write("Es agosto");
+}
+
+// La mejor opcion es convirtiendo la cadena de caracteres en minusculas.

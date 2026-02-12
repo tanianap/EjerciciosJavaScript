21451a4a083b3caa8810983eb64f3571618725b2
tanianap <qyj26808@educastur.es>
2026-02-12 11:24:42 +0100

    Ejercicios JS 19

---
+++ js/miscript19.js
@@ -0,0 +1,18 @@
+var coches = ["BMW", "Seat", "Renault", "Mercedes", "Peugeot", "Citroen"];
+
+// Recorrer Array con For
+for (let i=0; i<coches.length;i++){
+    alert(coches[i]);
+}
+
+var frutas = ["Manzana", "Pera", "Naranja", "Plátano", "Melocotón"];
+
+// Recorrer Array con For utilizando lo selementos del Array
+for (let i=0; i<frutas.length;i++){
+    alert(frutas[i]);
+}
+
+var paises = ["España", "Francia", "Italia", "Portugal", "Alemania"];
+
+// Recorrer array con forEach
+paises.forEach(pais => alert(pais));
